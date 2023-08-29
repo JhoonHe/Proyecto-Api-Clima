5c9c1a6896daec8428d9430beabd5ba317723457
Jhon Stiven Arcila Herrera <110495088+JhoonHe@users.noreply.github.com>
2023-08-29 13:09:33 -0500

    Update home.component.ts

--- Frontend/src/app/components/home/home.component.ts
+++ Frontend/src/app/components/home/home.component.ts
@@ -17,18 +17,15 @@ export class HomeComponent implements OnInit {
   url_Api: string = 'https://api.openweathermap.org/data/2.5/weather?appid';
   key_Api: string = 'caffae02abcf570a374727be813eeefd';
 
-  place: string = 'armenia';
+  place: string = 'Bogota';
   country: string = 'CO';
   fecha: any;
   hora: any;
   data: any = '';
   weather: string = '';
-  // weatherIcon: string = "";
   weatherIconUrl: string = '';
   backgroundColor: string = '';
-  // temp: number = 0;
-  // tempCelsius: string = "";
-  clock: BehaviorSubject<Date> = new BehaviorSubject<Date>(new Date());
+
   countries: any = countries;
   currentTime: any;
   form: FormGroup;
@@ -57,9 +54,7 @@ export class HomeComponent implements OnInit {
       .subscribe(
         {
           next: (response: any) => {
-            // console.log(response);
             this.data = response;
-            // console.log(response);
 
             this.checkWeather(
               response.weather[0].main,
@@ -70,31 +65,12 @@ export class HomeComponent implements OnInit {
             console.log(error);
           },
         }
-        /*
-      ((response: any) => {
-        // console.log(response);
-        this.data = response;
-
-        this.checkWeather(response.weather[0].main, response.weather[0].icon);
-
-
-      }),
-      ((error: any) => {
-        console.log(error);
-
-      })
-      */
       );
   }
 
   onSubmit() {
     if (this.form.valid) {
-      // console.log("Hola");
-      // console.log(this.place);
-      // console.log(this.country);
-
       this.getTime(this.form.value.country);
-      // console.log('Onsubmit', this.countries[0].code);
 
       this.client
         .getRequest(
@@ -102,7 +78,6 @@ export class HomeComponent implements OnInit {
         )
         .subscribe({
           next: (response: any) => {
-            // console.log(response);
             this.data = response;
             this.checkWeather(
               response.weather[0].main,
@@ -127,103 +102,72 @@ export class HomeComponent implements OnInit {
   }
 
   checkWeather = (weather: string, weatherIcon: string) => {
-    // console.log("a", weatherIcon);
     if (weather === 'Thunderstorm') {
-      // console.log(1);
       this.weather = 'Tormenta eléctrica';
       this.weatherIconUrl = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
     } else if (weather === 'Drizzle') {
-      // console.log(2);
       this.weather = 'Llovizna';
       this.weatherIconUrl = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
     } else if (weather === 'Rain') {
-      // console.log(3);
       this.weather = 'Lluvia';
       this.weatherIconUrl = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
     } else if (weather === 'Snow') {
-      // console.log(4);
       this.weather = 'Nieve';
       this.weatherIconUrl = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
     } else if (weather === 'Mist') {
-      // console.log(5);
       this.weather = 'Niebla';
       this.weatherIconUrl = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
     } else if (weather === 'Smoke') {
-      // console.log(6);
       this.weather = 'Humo';
       this.weatherIconUrl = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
     } else if (weather === 'Haze') {
-      // console.log(7);
       this.weather = 'Neblina';
       this.weatherIconUrl = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
     } else if (weather === 'Dust') {
-      // console.log(8);
       this.weather = 'Polvo en suspensión';
       this.weatherIconUrl = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
     } else if (weather === 'Fog') {
-      // console.log(9);
       this.weather = 'Niebla densa';
       this.weatherIconUrl = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
     } else if (weather === 'Sand') {
-      // console.log(10);
       this.weather = 'Arena en suspensión';
       this.weatherIconUrl = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
     } else if (weather === 'Ash') {
-      // console.log(11);
       this.weather = 'Ceniza en suspensión';
       this.weatherIconUrl = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
     } else if (weather === 'Squall') {
-      // console.log(12);
       this.weather = 'Ráfaga';
       this.weatherIconUrl = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
     } else if (weather === 'Tornado') {
-      // console.log(13);
       this.weather = 'Tornado';
       this.weatherIconUrl = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
     } else if (weather === 'Clear') {
-      // console.log(14);
       this.weather = 'Cielo despejado';
       this.weatherIconUrl = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
     } else if (weather === 'Clouds') {
-      // console.log(15);
       this.weather = 'Nubes';
       this.weatherIconUrl = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
     }
   };
 
-  // enviarDatos(area: string, place: string) {
-  //   console.log(area);
-  //   console.log(place);
-  // }
-
   getTime(code: string) {
-    // console.log(this.place == 'armenia');
-    // console.log(code);
 
     this.time.forEach((e) => {
       if (code === e.country) {
-        // console.log(code, 'code');
-        // console.log(e.country, 'country');
-
         this.area = e.area;
         this.place = e.place;
       }
     });
 
     setInterval(() => {
-      // if (this.place == 'armenia') {
-      //   this.clock.next(new Date());
-      //   this.currentTime = this.clock.value;
-      // } else {
       this.client.getInfo(this.area, this.place).subscribe({
         next: (data) => {
-          // console.log(data.datetime);
           this.currentTime = '';
           this.hora = data.datetime.slice(11, 19);
           this.fecha = data.datetime.slice(0, 10);
         },
       });
-      // }
     }, 1000);
   }
+  
 }
