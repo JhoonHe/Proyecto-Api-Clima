a233dfd7ded4858eedf82907d96b57076e1333bc
Jhon Stiven Arcila Herrera <110495088+JhoonHe@users.noreply.github.com>
2023-09-05 14:53:22 -0500

    Update login-controller.js

--- Backend/controllers/login-controller.js
+++ Backend/controllers/login-controller.js
@@ -3,14 +3,19 @@ const generateToken = require("../helpers/generator-tokens");
 
 let login = (req, res) => {
   let email = req.body.email;
+  let password = req.body.password;
 
-  let token = generateToken(
-    { email: email },
-    signingKey.KEY_TOKEN,
-    new Date().getTime() + 2 * 60 * 1000
-  );
+  if (email === "test@gmail.com" && password === "12345") {
 
-  return res.status(200).json({ Status: "ok", token: token });
+    let token = generateToken(
+      { email: email },
+      signingKey.KEY_TOKEN,
+      new Date().getTime() + 2 * 60 * 1000
+    );
+    return res.status(200).json({ Status: "ok", token: token });
+  }
+
+  return res.status(400).json({ Status: "error" });
 };
 
 module.exports = {
